0fe08bf550e4cdcbe6f66143456c68832e339e47
Tim Renton <tim.renton@bt.com>
2020-06-11 11:01:51 +1000

    adding train and predict functionality

--- test.py
+++ test.py
@@ -5,21 +5,32 @@ from sklearn.datasets import load_iris
 iris = load_iris()
 df = pd.DataFrame(iris['data'], columns=iris['feature_names'])
 
-data = {
-    'model_id': 'model',
-    'data': df.loc[[0], :].to_dict()
+X = df.to_dict()
+
+sample = df.loc[[0], :].to_dict()
+
+df['labels'] = [iris['target_names'][t] for t in iris['target']]
+y = df[['labels']].to_dict()
+
+train_data = {
+    'model_type': 'LogisticRegression',
+    'X': X,
+    'y': y
 }
 
+response = requests.post('http://127.0.0.1:5000/train', json=train_data)
 
-# x = df.loc[[0], :].to_dict()
+print('Train Reponse Code:', response.status_code)
 
-# print(x)
+if response.status_code == 200:
+    print(response.json())
 
+    model_id = response.json()['model_id']
 
-# # tmp = pd.DataFrame.from_dict(x)
-# tmp = pd.DataFrame(x)
-
-# print(tmp)
+data = {
+    'model_id': model_id,
+    'data': sample
+}
 
 response = requests.post('http://127.0.0.1:5000/predict', json=data)
 
